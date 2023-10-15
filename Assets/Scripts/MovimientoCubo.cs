516ae8248599d6d135ed8a59cbbee15cb5d9ba94
SrCheese44 <sr.cheese44@gmail.com>
2023-10-15 22:43:28 +0100

    Movimiento basico, monedas y final de nivel
    movimiento, salto, código de monedas y pantalla de final de nivel

--- Assets/Scripts/MovimientoCubo.cs
+++ Assets/Scripts/MovimientoCubo.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
 public class MovimientoCubo : MonoBehaviour
 {
 
     Rigidbody rb;
+
+
     private float movX, movZ;
-    private bool haSaltado = false;
+    private bool estaSaltando;
+    public float JumpPower;
     public float speed = 15;
+    
+    private int ContadorMonedas = 0;
+    private int MonedasFinal = 3;
+
+    public GameObject NivelEnd;
+    public TMP_Text TextoNivelEnd;
+    public TMP_Text textoContador;
+    private float tiempo;
+
+    public AudioSource Musica;
+    public AudioSource FX;
+
     void Start()
     {
        rb = GetComponent<Rigidbody>();
@@ -18,11 +35,23 @@ public class MovimientoCubo : MonoBehaviour
     {
         movX = Input.GetAxis("Horizontal");
         movZ = Input.GetAxis("Vertical");
-        if (Input.GetButtonDown("Jump"))
-        {
-            haSaltado = true;
 
+        if (Input.GetButtonDown("Jump")&& estaSaltando == false)
+        {
+            estaSaltando = true;
+            rb.AddForce(rb.velocity.x, JumpPower, rb.velocity.z, ForceMode.Impulse);
         }
+
+        if(ContadorMonedas == MonedasFinal)
+        {
+            NivelEnd.SetActive(true);
+            Time.timeScale = 0;
+            mensajefinal();
+            Musica.Stop();
+           
+        }
+
+        contador();
     }
 
 
@@ -31,13 +60,58 @@ public class MovimientoCubo : MonoBehaviour
 
         Vector3 NuevaVelocidad = new Vector3(movX * speed, rb.velocity.y, movZ * speed);
         rb.velocity = NuevaVelocidad;
-        /*
-        if (haSaltado)
-        {
-
-        }
-        */
+       
     }
+
+
+    void contador()
+    {
+        tiempo += Time.deltaTime;
+        textoContador.text = tiempo.ToString("#0.00");
+    }
+
+
+
+
+
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("moneda"))
+        {
+            ContadorMonedas++;
+            Destroy(col.gameObject);
+            FX.Play();
+        }
+
+
+    }
+
+
+    private void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.CompareTag("Suelo"))
+        {
+            estaSaltando = false; 
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Suelo"))
+        {
+            estaSaltando = true;
+        }
+    }
+
+    void mensajefinal()
+    {
+        TextoNivelEnd.text = "¡Nivel Compleatdo!";
+    }
+
+
+
+
 }
 
     
